5c4d7f475a0288a8e62aae207d5cdab86ca130c6
Lethal15004 <Phamvahuy2004@gmail.com>
2024-05-30 22:11:30 +0700

    Add file pug

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express') //Nhúng express vào dự án
-const app = express() // Khởi tạo ưng dụng web sử dụng express
-const port = 1504
+const express = require('express');//Nhúng express vào dự án
+const app = express(); // Khởi tạo ưng dụng web sử dụng express
+const port = 1504;
 
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.get('/', (req,res) =>{
-
+    res.render('client/pages/home/index');
 })
 
 app.get('/products', (req,res) =>{
-
+    res.render('client/pages/products/index');
 })
 
 app.listen(port, () => {
     console.log(`Đang lắng nghe cổng http://localhost:${port}`);
-  })
+})
