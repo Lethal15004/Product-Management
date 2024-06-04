cff9ff45680ef65e05eb3965c15afec688a963cb
Lethal15004 <Phamvahuy2004@gmail.com>
2024-06-04 21:32:52 +0700

    Add .env

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');//Nhúng express vào dự án
 const app = express(); // Khởi tạo ưng dụng web sử dụng express
-const port = 1504;
+const routeClient=require('./routes/client/index.route');//Nhúng route vào dự án
+require('dotenv').config()
 
+// Chú ý để nhúng file vào dự án thì cần phải sử dụng đúng đường dẫn và require
+// Chú ý đường dẫn cùng cấp thì mặc định bắt đầu là ./ . Từ con ra cha là ../ . 
+// Chú ý render thì đường dẫn nó đã mặc định ./view + đường dẫn 
+    // Example: res.render('client/pages/home/index'); sẽ là /views/client/pages/home/index.pug
+// Chú ý folder .env để chứa các biến môi trường (port, database, secret key, ...)
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.get('/', (req,res) =>{
-    res.render('client/pages/home/index');
-})
+routeClient.index(app);
 
-app.get('/products', (req,res) =>{
-    res.render('client/pages/products/index');
-})
-
-app.listen(port, () => {
-    console.log(`Đang lắng nghe cổng http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Đang lắng nghe cổng http://localhost:${process.env.PORT}`);
 })
