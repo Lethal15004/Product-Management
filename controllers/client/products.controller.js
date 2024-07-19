0cd93f42bd1fb7f783a97c565fd8153fe3c71abb
Lethal15004 <Phamvahuy2004@gmail.com>
2024-07-19 18:13:17 +0700

    Trang danh sách sản phẩm bên client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,6 +1,6 @@
 const Products=require('../../models/admin/product.model');
+const categoryProducts=require('../../models/admin/product-category.model');
 module.exports.index= async (req,res)=>{
-    console.log(res.locals.categories);
     const listProducts= await Products.find({
         status:'active',
         deleted:false
@@ -30,4 +30,46 @@ module.exports.detail= async (req,res)=>{
         req.flash('error','Không tìm thấy sản phẩm');
         res.redirect('/products');
     }
+}
+
+module.exports.category= async (req,res)=>{
+    const slug= req.params.slug;
+    const allCategoriesFind=[];
+
+    const category = await categoryProducts.findOne({
+        status:'active',
+        deleted:false,
+        slug:slug
+    }).select('parent_id');
+    allCategoriesFind.push(category.id);
+
+    const getSubCategory = async (currentId)=>{
+        const listCategories= await categoryProducts.find({
+            parent_id:currentId,
+            status:'active',
+            deleted:false
+        })
+        for(const category of listCategories){
+            allCategoriesFind.push(category.id);
+            await getSubCategory(category.id);
+        }
+    }
+    await getSubCategory(category.id);
+
+
+    console.log(allCategoriesFind);
+    const listProducts= await Products.find({
+        product_category_id:{
+            $in:allCategoriesFind
+        },
+        status:'active',
+        deleted:false
+    }).select('-description')
+    for(const item of listProducts){
+        item.newPrice=((1-item.discountPercentage/100)*item.price).toFixed(0);
+    }
+    res.render('client/pages/products/index',{
+        title:'Danh sách sản phẩm',
+        listProducts:listProducts
+    })
 }
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -3,4 +3,5 @@ const router=express.Router();
 const productController=require('../../controllers/client/products.controller')
 router.get('/',productController.index);
 router.get('/detail/:slug',productController.detail);
+router.get('/:slug',productController.category);
 module.exports=router;
