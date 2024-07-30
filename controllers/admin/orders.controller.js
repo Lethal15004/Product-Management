d389deceac5b7a079b6675d2efba72c8dafa68b9
Lethal15004 <Phamvahuy2004@gmail.com>
2024-07-30 14:43:18 +0700

    Trang chi tiết đơn hàng

--- controllers/admin/orders.controller.js
+++ controllers/admin/orders.controller.js
@@ -4,17 +4,45 @@ const Product=require('../../models/admin/product.model');
 const moment = require('moment');  
 module.exports.index= async (req,res)=>{
     const orders= await Order.find();
-    for(const product of orders){
+    for(const order of orders){
         let totalMoney=0;
-        for(const item of product.products){
+        for(const item of order.products){
             const priceNew=((1-item.discountPercentage/100)*item.price).toFixed(0);
             totalMoney+=priceNew*item.quantity;
         }
-        product.totalMoney=totalMoney;
-        product.createdAtFormat=moment(product.createdAt).format('DD/MM/YYYY HH:mm:ss');
+        order.totalMoney=totalMoney;
+        order.createdAtFormat=moment(order.createdAt).format('DD/MM/YYYY HH:mm:ss');
     }
     res.render('admin/pages/orders/index',{
         title:"Danh sách đơn hàng",
         orders:orders
     })
+}
+
+module.exports.detail= async (req,res)=>{
+    try {
+        const id =req.params.id;
+        const order= await Order.findOne({_id:id});
+        const listProducts=[];
+        let totalMoney=0;
+
+        for(const item of order.products){
+            const productInfo=await Product.findOne({_id:item.productId,deleted:false,status:'active'}).select('title price discountPercentage thumbnail slug')
+            const priceNew=((1-item.discountPercentage/100)*item.price).toFixed(0);
+            productInfo.priceNew=priceNew;
+            productInfo.quantity=item.quantity;
+            productInfo.totalPrice=priceNew*item.quantity;
+            listProducts.push(productInfo);
+            totalMoney+=priceNew*item.quantity;
+        }
+        order.totalMoney=totalMoney;
+        res.render('admin/pages/orders/order-detail',{
+            title:"Chi tiết đơn hàng",
+            order:order,
+            listProducts:listProducts
+        })
+    } catch (error) {
+        req.flash('error','Lỗi: '+error);
+        res.redirect('/admin/orders');
+    }
 }
--- routes/admin/orders.route.js
+++ routes/admin/orders.route.js
@@ -2,5 +2,5 @@ const express=require('express');
 const router=express.Router();
 const orderController=require('../../controllers/admin/orders.controller');
 router.get('/',orderController.index);
-
+router.get('/detail/:id',orderController.detail);
 module.exports=router; 
