9b63576be066cc50844019962f8f021fb225efd2
Lethal15004 <Phamvahuy2004@gmail.com>
2024-08-03 14:58:21 +0700

    Trang Dashboard bên admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,22 @@
-module.exports=(req,res)=>{
+const Category=require('../../models/admin/product-category.model');
+const Product=require('../../models/admin/product.model');
+const Account=require('../../models/admin/account.model');
+const User=require('../../models/client/user.model');
+module.exports.index= async (req,res)=>{
+    const modelArray=[Category,Product,Account,User];
+    const statistic={}
+    for(const model of modelArray){
+        const total= await model.countDocuments({deleted:false});
+        const active= await model.countDocuments({status:'active',deleted:false});
+        const inactive= await model.countDocuments({status:'inactive',deleted:false});
+        statistic[model.modelName]={
+            total:total,
+            active:active,
+            inactive:inactive
+        }
+    }
     res.render('admin/pages/dashboard/index',{
-        title:'Dashboard'
+        title:'Dashboard',
+        statistic:statistic
     });
 }
--- routes/admin/dashboard.route.js
+++ routes/admin/dashboard.route.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
 const dashboardController=require('../../controllers/admin/dashboard.controller')
-router.get('/',dashboardController)
+router.get('/',dashboardController.index)
 
 module.exports=router;
