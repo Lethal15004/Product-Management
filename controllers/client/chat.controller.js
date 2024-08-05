75c84b2fa476307073e199ded107e27dc6964102
Lethal15004 <Phamvahuy2004@gmail.com>
2024-08-05 20:03:42 +0700

    Làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,9 +1,25 @@
+const Chat=require('../../models/client/chat.model');
+const User=require('../../models/client/user.model');
 module.exports.pageChat=async(req,res)=>{
     //Socket io
-    _io.on('connection', (socket) => {
+    _io.once('connection', (socket) => {
+        socket.on('CLIENT_SEND_MESSAGE', async (message) => {
+            const data={
+                userId:res.locals.user.id,
+                content:message
+            }
+            const chat=new Chat(data);
+            await chat.save();
+        })
     })
+    const chats=await Chat.find({});
+    for(const chat of chats){
+        const user=await User.findOne({_id:chat.userId}).select('fullName');
+        chat.fullName=user.fullName;
+    }
     //End socket io
     res.render('client/pages/chat/index.pug',{
         title:'Chat',
+        chats:chats
     })
 }
--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -1,1 +1,10 @@
 var socket=io();
+const formSendMessage=document.querySelector('[send-message]');
+formSendMessage.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=e.target.elements[0].value;
+    if(message.trim()!==''){
+        socket.emit('CLIENT_SEND_MESSAGE',message);
+        e.target.elements[0].value='';
+    }
+})
