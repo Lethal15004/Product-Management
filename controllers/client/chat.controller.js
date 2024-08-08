db135a60dd85466f789f7cc1e114a77de5312a22
Lethal15004 <Phamvahuy2004@gmail.com>
2024-08-08 16:36:53 +0700

    Hiển thị data realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -2,16 +2,25 @@ const Chat=require('../../models/client/chat.model');
 const User=require('../../models/client/user.model');
 module.exports.pageChat=async(req,res)=>{
     //Socket io
+    
     _io.once('connection', (socket) => {
+        //Receive message from client
         socket.on('CLIENT_SEND_MESSAGE', async (message) => {
             const data={
                 userId:res.locals.user.id,
-                content:message
+                content:message,
             }
             const chat=new Chat(data);
             await chat.save();
+
+            _io.emit('SERVER_RETURN_MESSAGE', {
+                userId:res.locals.user.id,
+                content:message,
+                fullName:res.locals.user.fullName
+            });
         })
     })
+
     const chats=await Chat.find({});
     for(const chat of chats){
         const user=await User.findOne({_id:chat.userId}).select('fullName');
--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -1,4 +1,13 @@
 var socket=io();
+
+//Sự kiện thanh kéo cho chat
+const bodyChat=document.querySelector('.chat .inner-body');
+if(bodyChat){
+    bodyChat.scrollTop=Number(bodyChat.scrollHeight) + 56 ;
+}
+
+
+//Sư kiện 1 phía
 const formSendMessage=document.querySelector('[send-message]');
 formSendMessage.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -7,4 +16,26 @@ formSendMessage.addEventListener('submit',(e)=>{
         socket.emit('CLIENT_SEND_MESSAGE',message);
         e.target.elements[0].value='';
     }
+    
 })
+
+//Sự kiện cho tất cả mọi nên viêt cẩn thận
+socket.on('SERVER_RETURN_MESSAGE', (data) => {
+    const idUserCurrent=document.querySelector('[my-id]').getAttribute('my-id');
+    const newDiv=document.createElement('div');
+    if(data.userId===idUserCurrent){
+        newDiv.classList.add('inner-outgoing');
+        newDiv.innerHTML=`<div class="inner-content">${data.content}</div>`
+    }else{
+        newDiv.classList.add('inner-incoming');
+        newDiv.innerHTML=`
+        <div class="inner-name">${data.fullName}</div>
+        <div class="inner-content">${data.content}</div>`
+    }
+    const innerBodyChat=document.querySelector('.chat .inner-body');
+    innerBodyChat.appendChild(newDiv);
+    if(data.userId===idUserCurrent){
+        innerBodyChat.scrollTop=innerBodyChat.scrollHeight
+    }
+})
+
